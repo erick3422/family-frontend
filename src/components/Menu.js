0b5be6feeb0bc1f4f2fe4c449f929e22441e49ab
erick3422 <erickloco882@yahoo.com>
2022-03-09 20:42:10 -0800

    2 Commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './menu.scss'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
   return (
     <>
-      <div>
-        <h1>Future Menu</h1>
+      <div className= 'product'>
+        <img src={require('../img/menudo.jpg').default }/>
+
+        <div className='product__info'>
+          <p className='info__name'>Product 1</p>
+          <p className='info_description'>
+            Here is a future description of the product
+          </p>
+
+          <p className='info__price'>$49.99</p>
+
+          <Link to={`/product/${1}`} className='info__ button'>View</Link>
+        </div>
       </div>
     </>
   )
---
+++ src/data/product.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    id: 1,
+    title: 'Menudo',
+    image: '/public/assets/products/menudo.jpg',
+    description: 'Here is a description of the food ',
+    price: 49.99,
+    countInStock: 12
+  }
+]
+
+export default products
