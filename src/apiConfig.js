e4b071f24719864dbc7594fd19c433432bf0fa1f
erick3422 <erickloco882@yahoo.com>
2022-03-09 20:52:51 -0800

    6 commit

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,10 +2,10 @@ let apiUrl
 // Be sure to put a "homepage" in package.json
 
 const apiUrls = {
-	// TODO: change production url after launch party
+  // TODO: change production url after launch party
 
-	production: 'https://young-mesa-85080.herokuapp.com',
-	development: 'http://localhost:4741',
+  production: 'https://young-mesa-85080.herokuapp.com',
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
