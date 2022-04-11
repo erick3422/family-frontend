e9977c5d9c5caa131b8436a524becaeb42d669e4
erick3422 <erickloco882@yahoo.com>
2022-04-11 14:33:28 -0700

    added Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import SubHeading from '../components/SubHeading/SubHeading'
+import { images } from '../constants'
 import '../screens/home.scss'
 const Home = () => {
   return (
     <>
-      <div className='title'>
-        Home Coming Soon
+      <div className='app__header app__wrapper section__padding' id='home'>
+        <div className='app__wrapper_info'>
+          <SubHeading title='Discover A New Flavor'/>
+          <h1 className='app__header-h1'>The key to<br></br> Fine Dining </h1>
+          <p className='p__opensans' style={{ margin: '2rem 0' }}> Here in Family believe that, the best food is serve with the most love ones next to you,<br></br>making an experience unique and taste it.</p>
+          <button type='button' className='custom__button'>Explore Menu</button>
+        </div>
+
+        <div className='app__wrapper_img'>
+          <img src={images.welcome} alt='header img' />
+        </div>
       </div>
     </>
   )
